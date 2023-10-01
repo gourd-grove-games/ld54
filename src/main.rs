7d7905fe6834d3609e08d39d06518da021c38c6b
Peyton-Spencer <pas1234567@gmail.com>
2023-10-01 01:00:59 -0600

    3d tilemap with PanOrbitCamera disable render feature of tilemap; just use it for tile tracking / parent child entities add skybox move emissive cube up y

--- src/main.rs
+++ src/main.rs
@@ -11,137 +11,45 @@ use bevy::{
     core_pipeline::Skybox,
     core_pipeline::{bloom::BloomSettings, tonemapping::Tonemapping},
     pbr::ScreenSpaceAmbientOcclusionBundle,
-    render::{
-        render_resource::{TextureViewDescriptor, TextureViewDimension},
-        renderer::RenderDevice,
-        texture::CompressedImageFormats,
-    },
+    render::render_resource::{TextureViewDescriptor, TextureViewDimension},
 };
-use bevy_ecs_tilemap::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_panorbit_camera::*;
-mod camera;
 mod tilemap;
-
-const CUBEMAPS: &[(&str, CompressedImageFormats)] = &[
-    ("textures/bsb.png", CompressedImageFormats::NONE),
-    (
-        "textures/Ryfjallet_cubemap_astc4x4.ktx2",
-        CompressedImageFormats::ASTC_LDR,
-    ),
-    (
-        "textures/Ryfjallet_cubemap_bc7.ktx2",
-        CompressedImageFormats::BC,
-    ),
-    (
-        "textures/Ryfjallet_cubemap_etc2.ktx2",
-        CompressedImageFormats::ETC2,
-    ),
-];
 
 fn main() {
     App::new()
-        // .init_resource::<Board>()
-        // .register_type::<BoardPos>()
-        // .register_type::<BaseTile>()
-        // .register_type::<Board>()
-        // .insert_resource(DirectionalLightShadowMap { size: 2048 })
-        // .insert_resource(ClearColor(Color::rgb_linear(0.5, 1.3, 1.9)))
-        // .insert_resource(DirectionalLightShadowMap { size: 2048 })
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: String::from("Permaculture Tycoon"),
-                        ..default()
-                    }),
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest()),
-        ) // .add_plugins(PanOrbitCameraPlugin)
-        // .add_plugins(TemporalAntiAliasPlugin)
+        .insert_resource(DirectionalLightShadowMap { size: 2048 })
+        .insert_resource(ClearColor(Color::rgb_linear(0.5, 1.3, 1.9)))
+        .insert_resource(DirectionalLightShadowMap { size: 2048 })
+        .insert_resource(AmbientLight {
+            color: Color::rgb_u8(210, 220, 240),
+            brightness: 1.0,
+        })
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from("Permaculture Tycoon"),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(PanOrbitCameraPlugin)
+        .add_plugins(TemporalAntiAliasPlugin)
         .add_plugins(tilemap::GroundMapPlugin)
         .add_plugins(WorldInspectorPlugin::default())
-        // .add_systems(Startup, spawn_camera)
-        // .add_systems(Startup, setup)
-        // .add_systems(
-        //     Update,
-        //     (cycle_cubemap_asset, asset_loaded.after(cycle_cubemap_asset)),
-        // )
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_emmissive_cube)
+        .add_systems(Update, png_metadata)
         .run();
 }
 
 #[derive(Resource)]
 struct Cubemap {
     is_loaded: bool,
-    index: usize,
     image_handle: Handle<Image>,
 }
 
-#[derive(Resource, Reflect)]
-#[reflect(Resource)]
-struct Board {
-    i_len: u32,
-    j_len: u32,
-}
-
-// todo deprecate
-impl Default for Board {
-    fn default() -> Self {
-        Board {
-            i_len: tilemap::BOARD_SIZE_X,
-            j_len: tilemap::BOARD_SIZE_Y,
-        }
-    }
-}
-
-impl Board {
-    fn spawn(&self, commands: &mut Commands, asset_server: &Res<AssetServer>) {
-        let cell_scene = asset_server.load("models/grass_tile.glb#Scene0");
-        for j in 0..self.i_len {
-            for i in 0..self.j_len {
-                let height = 0.0; //rand::thread_rng().gen_range(-0.1..0.1);
-                commands.spawn((
-                    SceneBundle {
-                        transform: Transform::from_xyz(
-                            i as f32 - (self.i_len as f32 / 2.0),
-                            height - 0.2,
-                            j as f32 - (self.j_len as f32 / 2.0),
-                        ),
-                        scene: cell_scene.clone(),
-                        ..default()
-                    },
-                    BaseTile::default(),
-                    BoardPos { i, j },
-                    name_tile(),
-                ));
-            }
-        }
-    }
-}
-
-fn name_tile() -> Name {
-    Name::new("Tile")
-}
-
-#[derive(Component, Reflect, Default, Debug)]
-#[reflect(Component)]
-enum BaseTile {
-    #[default]
-    Grass,
-    Stone,
-    Wood,
-}
-
-#[derive(Component, Reflect, Default)]
-#[reflect(Component)]
-struct BoardPos {
-    i: u32,
-    j: u32,
-}
-
 fn spawn_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             illuminance: 20000.0,
@@ -157,117 +65,38 @@ fn spawn_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..default()
     });
 
-    // let skybox_handle = asset_server.load(CUBEMAPS[0].0);
-
-    // ambient light
-    // NOTE: The ambient light is used to scale how bright the environment map is so with a bright
-    // environment map, use an appropriate color and brightness to match
-    commands.insert_resource(AmbientLight {
-        color: Color::rgb_u8(210, 220, 240),
-        brightness: 1.0,
-    });
-
-    // commands
-    //     .spawn((
-    //         Camera3dBundle {
-    //             camera: Camera {
-    //                 hdr: true,
-    //                 ..default()
-    //             },
-    //             tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
-    //             transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
-    //             ..default()
-    //         },
-    //         BloomSettings::default(),
-    //         PanOrbitCamera::default(),
-    //         Skybox(skybox_handle.clone()),
-    //     ))
-    //     .insert(ScreenSpaceAmbientOcclusionBundle::default())
-    //     .insert(TemporalAntiAliasBundle::default());
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
-    board: Res<Board>,
-) {
-    let skybox_handle = asset_server.load(CUBEMAPS[0].0);
-    board.spawn(&mut commands, &asset_server);
-    let material_emissive1 = materials.add(StandardMaterial {
-        emissive: Color::rgb_linear(13.99, 5.32, 2.0), // 4. Put something bright in a dark environment to see the effect
-        ..default()
-    });
-
-    // let grass_scene = asset_server.load("models/grass_tile.glb#Scene0");
-    // let stone_scene = asset_server.load("models/stone_tile.glb#Scene0");
-    // let wood_scene = asset_server.load("models/wood_tile.glb#Scene0");
-    // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: material_emissive1,
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-
+    let skybox_handle: Handle<Image> = asset_server.load("textures/Ryfjallet_cubemap.png");
     commands.insert_resource(Cubemap {
         is_loaded: false,
-        index: 0,
-        image_handle: skybox_handle,
+        image_handle: skybox_handle.clone(),
     });
+
+    commands
+        .spawn((
+            Camera3dBundle {
+                camera: Camera {
+                    hdr: true,
+                    ..default()
+                },
+                tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+                transform: Transform::from_xyz(8.0, 4.5, 8.0),
+                ..default()
+            },
+            BloomSettings::default(),
+            PanOrbitCamera { ..default() },
+            Skybox(skybox_handle),
+        ))
+        .insert(ScreenSpaceAmbientOcclusionBundle::default())
+        .insert(TemporalAntiAliasBundle::default());
 }
 
-const CUBEMAP_SWAP_DELAY: f32 = 3.0;
-
-fn cycle_cubemap_asset(
-    time: Res<Time>,
-    mut next_swap: Local<f32>,
-    mut cubemap: ResMut<Cubemap>,
-    asset_server: Res<AssetServer>,
-    render_device: Res<RenderDevice>,
-) {
-    let now = time.elapsed_seconds();
-    if *next_swap == 0.0 {
-        *next_swap = now + CUBEMAP_SWAP_DELAY;
-        return;
-    } else if now < *next_swap {
-        return;
-    }
-    *next_swap += CUBEMAP_SWAP_DELAY;
-
-    let supported_compressed_formats =
-        CompressedImageFormats::from_features(render_device.features());
-
-    let mut new_index = cubemap.index;
-    for _ in 0..CUBEMAPS.len() {
-        new_index = (new_index + 1) % CUBEMAPS.len();
-        if supported_compressed_formats.contains(CUBEMAPS[new_index].1) {
-            break;
-        }
-        info!("Skipping unsupported format: {:?}", CUBEMAPS[new_index]);
-    }
-
-    // Skip swapping to the same texture. Useful for when ktx2, zstd, or compressed texture support
-    // is missing
-    if new_index == cubemap.index {
-        return;
-    }
-
-    cubemap.index = new_index;
-    cubemap.image_handle = asset_server.load(CUBEMAPS[cubemap.index].0);
-    cubemap.is_loaded = false;
-}
-
-fn asset_loaded(
+fn png_metadata(
     asset_server: Res<AssetServer>,
     mut images: ResMut<Assets<Image>>,
     mut cubemap: ResMut<Cubemap>,
-    mut skyboxes: Query<&mut Skybox>,
 ) {
     if !cubemap.is_loaded && asset_server.get_load_state(&cubemap.image_handle) == LoadState::Loaded
     {
-        info!("Swapping to {}...", CUBEMAPS[cubemap.index].0);
         let image = images.get_mut(&cubemap.image_handle).unwrap();
         // NOTE: PNGs do not have any metadata that could indicate they contain a cubemap texture,
         // so they appear as one texture. The following code reconfigures the texture as necessary.
@@ -280,11 +109,25 @@ fn asset_loaded(
                 ..default()
             });
         }
-
-        for mut skybox in &mut skyboxes {
-            skybox.0 = cubemap.image_handle.clone();
-        }
-
         cubemap.is_loaded = true;
     }
+}
+
+fn spawn_emmissive_cube(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let material_emissive1 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(13.99, 5.32, 2.0), // 4. Put something bright in a dark environment to see the effect
+        ..default()
+    });
+
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: material_emissive1,
+        transform: Transform::from_xyz(0.0, 2.5, 0.0),
+        ..default()
+    });
 }
--- src/tilemap.rs
+++ src/tilemap.rs
@@ -1,18 +1,14 @@
-use std::f32::consts::PI;
-
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 
-use crate::camera;
-
-pub const BOARD_SIZE_X: u32 = 15;
-pub const BOARD_SIZE_Y: u32 = 15;
+pub const BOARD_SIZE_I: u32 = 8;
+pub const BOARD_SIZE_J: u32 = 8;
 pub struct GroundMapPlugin;
 impl Plugin for GroundMapPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(TilemapPlugin)
             .add_systems(Startup, spawn_tilemap)
-            .add_systems(Update, camera::movement)
+            // .add_systems(Update, camera::movement)
             .add_systems(Update, gizmos);
     }
 }
@@ -24,53 +20,60 @@ fn gizmos(mut gizmos: Gizmos) {
 }
 
 fn spawn_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_rotation(Quat::from_rotation_x(PI / 3.0)),
-        projection: OrthographicProjection {
-            far: 1000.0,
-            near: -1000.0,
-            scaling_mode: ScalingMode::WindowSize(5.0),
-            ..default()
-        },
-        ..default()
-    });
-    let grass_tex_handle: Handle<Image> = asset_server.load("textures/grass.png");
     let map_size = TilemapSize {
-        x: BOARD_SIZE_X,
-        y: BOARD_SIZE_Y,
+        x: BOARD_SIZE_I,
+        y: BOARD_SIZE_J,
     };
 
     // TilemapBundle requires TileStorage component; a grid of tile entities
     // Create TileStorage with pre-allocated capacity
     let mut tile_storage = TileStorage::empty(map_size);
-    let map_type = TilemapType::Isometric(IsoCoordSystem::Diamond);
+    let map_type = TilemapType::Square;
 
     // Create empty tilemap entity
     // added to each tile as tilemap_id component
     let tilemap_entity = commands.spawn_empty().id();
+    let cell_scene = asset_server.load("models/grass_tile.glb#Scene0");
+    commands.entity(tilemap_entity).with_children(|parent| {
+        for x in 0..map_size.x {
+            for y in 0..map_size.y {
+                let tile_pos = TilePos { x, y };
+                let tile_entity = parent
+                    .spawn((
+                        TileBundle {
+                            position: tile_pos,
+                            tilemap_id: TilemapId(tilemap_entity),
+                            ..Default::default()
+                        },
+                        SceneBundle {
+                            scene: cell_scene.clone(),
+                            transform: Transform::from_xyz(x as f32, 0.0, y as f32),
+                            ..default()
+                        },
+                        Name::new("Tile"),
+                    ))
+                    .id();
+                tile_storage.set(&tile_pos, tile_entity);
+            }
+        }
+    });
 
-    // Fill the tile storage with tiles
-    helpers::filling::fill_tilemap(
-        TileTextureIndex(75),
-        map_size,
-        TilemapId(tilemap_entity),
-        &mut commands,
-        &mut tile_storage,
-    );
-
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let tile_size = TilemapTileSize { x: 1.0, y: 1.0 };
     let grid_size = tile_size.into();
 
     // Insert TilemapBundle to the tilemap entity
     commands.entity(tilemap_entity).insert((
-        TilemapBundle {
+        bevy_ecs_tilemap::StandardTilemapBundle {
             grid_size,
             tile_size,
             size: map_size,
             map_type,
             storage: tile_storage,
-            texture: TilemapTexture::Single(grass_tex_handle),
-            transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+            transform: Transform::from_xyz(
+                (BOARD_SIZE_I - 1) as f32 / -2.0,
+                0.0,
+                (BOARD_SIZE_J - 1) as f32 / -2.0,
+            ),
             ..default()
         },
         Name::new("Ground Tilemap"),
